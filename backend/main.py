939f42f99545f6b235ae69bd7aba691abbb0ac8c
AnasNaveed69 <buttanas813@gmail.com>
2026-05-02 01:40:32 +0500

    cors policy updated

--- backend/main.py
+++ backend/main.py
@@ -81,7 +81,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://threxia.vercel.app",
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
